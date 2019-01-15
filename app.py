21ff78a18252cfc1cd68fe8f2c7ab1dfc75bea65
JunLin <183457U@mymail.nyp.edu.sg>
2019-01-15 19:31:14 +0800

    SDL

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-@app.route('/')
+from flask import *
+
+@app.route('/home')
 def Home():
     return render_template('heroku-testing.html', title='Home')
 
-if __name__ =  '__main__':
+if __name__ ==  '__main__':
     app.run(port='80')
